c059b9d725e46387d301d2e796854ef210dee4ae
Matias Wilkman <matias.wilkman@gmail.com>
2023-09-24 08:38:47 +0300

    switched to limnoria's built-in web server

--- UserList/plugin.py
+++ UserList/plugin.py
@@ -1,4 +1,4 @@
-from supybot import utils, plugins, ircutils, callbacks, irclib, world
+from supybot import utils, plugins, ircutils, callbacks, irclib, world, httpserver
 import supybot.log as log
 from supybot.commands import *
 from supybot.i18n import PluginInternationalization
@@ -11,6 +11,41 @@ import datetime
 
 _ = PluginInternationalization('UserList')
 
+class UserListServerCallback(httpserver.SupyHTTPServerCallback):
+    name = 'UserList'
+    defaultResponse = """
+        This plugin handles only GET request, please don't use other requests.
+        Content served: userlist.html, userlist.json"""
+
+    def doGet(self, handler, path):
+        if path == './userlist.html' or path == './userlist.json':
+            with open(path) as f:
+                response = f.read()
+        else:
+             handler.send_response(404) # Not found
+             handler.send_header('Content-type', 'text/html') # This is the MIME for HTML data
+             handler.end_headers() # We won't send more headers
+             handler.wfile.write(b"""
+             <!DOCTYPE html>
+             <html>
+              <head>
+               <meta charset="UTF-8">
+               <title>Error</title>
+              </head>
+              <body>
+               <h1>404 Not found</h1>
+               <p>
+                The document could not be found. Try one of this links:
+                <a href="./userlist.html">User List</a>
+                <a href="./userlist.json">User List, JSON format</a>
+               </p>
+              </body>
+             </html>""")
+             return
+        handler.send_response(200)
+        handler.send_header('Content-type', 'text/plain') # This is the MIME for plain text
+        handler.end_headers() # We won't send more headers
+        handler.wfile.write(response)
 
 class UserList(callbacks.Plugin):
     userlist = {}
@@ -18,6 +53,14 @@ class UserList(callbacks.Plugin):
         super().__init__(irc)
         self.updatelist(irc, None)
         self.renderlist()
+        #register http server callback:
+        callback = UserListServerCallback()
+        httpserver.hook('userlist', callback)
+
+    def die(self):
+        #unregister callback:
+        httpserver.unhook('userlist')
+        super().die()
 
     """List users from channels that the bot is on"""
     def updatelist(self, irc, msg):
@@ -50,7 +93,8 @@ str(self.registryValue("channels")))
         title.appendChild(dom.createTextNode("User List"))
         html.appendChild(title)
         html.appendChild(dom.createTextNode("User list created at " +
-str(datetime.datetime.now())))
+                                            str(datetime.datetime.now()
+                                                .strftime("%d.%m.%Y, %I:%M:%S"))))
         for channel in self.userlist.keys():
             table = dom.createElement("table")
             tr = dom.createElement("tr")
