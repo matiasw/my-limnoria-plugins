4adae418a9443d16641d42177a5594467cb24a25
Matias Wilkman <matias.wilkman@gmail.com>
2023-09-26 00:07:41 +0300

    support for multiple named calendars

--- Calendar/plugin.py
+++ Calendar/plugin.py
@@ -8,30 +8,50 @@ from supybot import utils, plugins, ircutils, callbacks
 from supybot.commands import *
 from supybot.i18n import PluginInternationalization
 from datetime import datetime
+import json
 import icalendar
 import requests
 
 _ = PluginInternationalization('Calendar')
 
+class iCal:
+    def nextevent(self):
+        for event in self.calendar.walk("VEVENT"):
+            date = icalendar.vDDDTypes.from_ical(event.get("DTSTART")) 
+            if date > datetime.now().date():
+                return event
+
+    def __init__(self, url):
+        self.calendar = icalendar.Calendar.from_ical(requests.get(url).text)
+        
 class Calendar(callbacks.Plugin):
     """Notifies of scheduled events in iCal calendars"""
     threaded = True
-
     
-    @wrap
-    def nextevent(self, irc, msg, args):
-        """takes no arguments
+    @wrap([additional("text")])    
+    def nextevent(self, irc, msg, args, name=""):
+        """takes name of calendar as argument, or uses first calendar if no name given
 
         Replies with the next event in the calendar
         """
-        calendar = icalendar.Calendar.from_ical(requests.get(self.registryValue("calendars")[0]).text)
-        for event in calendar.walk("VEVENT"):
-            date = icalendar.vDDDTypes.from_ical(event.get("DTSTART")) 
-            if date > datetime.now().date():
-                reply = "The next event in the calendar is {summary}, on {date}".format(
-                    summary=event.get("SUMMARY"),
-                    date=date.strftime("%d.%m.%Y"))
-                irc.reply(reply)
+        try:
+            calendars = json.loads(str(self.registryValue("calendars").replace('\\', '')))
+        except Exception as e:
+            irc.error("Could not parse the JSON in the calendars configuration value:" + str(e))
+            return
+        if name in calendars.keys():
+            ical = iCal(calendars[name])
+        elif calendars:
+            ical = iCal(calendars[list(calendars.keys())[0]])
+        else:
+            irc.error("No calendars configured.")
+            return
+        event = ical.nextevent()
+        date = icalendar.vDDDTypes.from_ical(event.get("DTSTART")) 
+        reply = "The next event in the calendar is {summary}, on {date}".format(
+            summary=event.get("SUMMARY"),
+            date=date.strftime(self.registryValue("dateformat")))
+        irc.reply(reply)
 
 Class = Calendar
 
